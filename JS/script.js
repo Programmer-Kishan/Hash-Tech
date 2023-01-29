0b7b93553faa0370a01266af25992d97cb7b6af4
kishan kathare <kishankathare45@gmail.com>
2023-01-29 12:59:33 +0530

    Text Container added

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 const backgroundContainer = document.getElementById('outer-container');
+const rotateShape = document.getElementById("circle");
 
 const btn = document.getElementById('btn');
 
@@ -9,4 +10,9 @@ btn.addEventListener('click', () => {
     backgroundContainer.classList.remove(color_class[ctr%3]);
     ctr++;
     backgroundContainer.classList.add(color_class[ctr%3]);
+
+    rotateShape.classList.add('rotate-animation');
+    setTimeout(() => {
+        rotateShape.classList.remove('rotate-animation');
+    }, 2000)
 })
