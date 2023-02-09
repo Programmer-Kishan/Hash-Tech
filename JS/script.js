23693a71bfb4e5f371ab346cabe1452a37dd68d3
kishan kathare <kishankathare45@gmail.com>
2023-02-10 00:30:29 +0530

    Added the Trainer section

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,27 @@
+
+// main container elements
 const backgroundContainer = document.getElementById('outer-container');
 const rotateShape = document.getElementById("shape");
 const image = document.getElementById("image");
 
 const btn = document.getElementById('btn');
 
-const title = document.getElementById('inner-title');
+const main_title = document.getElementById('inner-title');
+
+// trainer container elements
+const trainerContainer = document.getElementById('trainer-container');
+const trainerImage = document.getElementById("trainer-img");
+const trainerCard = document.getElementById('trainer-card');
+const trainerDetailContainer = document.getElementById('trainer-detail-box');
+const profName = document.getElementById("prof-name");
+const profQualification = document.getElementById('prof-qualification');
+const profImage = document.getElementById("prof-image");
 
 let color_class = ['green-scheme', 'blue-scheme', 'yellow-scheme'];
 let title_list = [
     {title: 'Java', width: '42%'}, 
     {title: 'C++', width: '33%'}, 
-    {title: 'Python', width: '62%'}
+    {title: 'Python', width: '64%'}
 ];
 let image_list = ['java.png', 'cpp.png', 'python.png'];
 let image_class = ['java-image', 'cpp-image', 'python-image'];
@@ -20,9 +31,10 @@ let PATH = './Images/'
 btn.addEventListener('click', () => {
     // for shape and balls color
     backgroundContainer.classList.remove(color_class[ctr%3]);
-    
+    trainerContainer.classList.remove(color_class[ctr%3]);
     ctr++;
     backgroundContainer.classList.add(color_class[ctr%3]);
+    trainerContainer.classList.add(color_class[ctr%3]);
     
 
     // for rotating shape
@@ -32,8 +44,8 @@ btn.addEventListener('click', () => {
     }, 2000)
 
     // for title of the main page
-    title.innerHTML = title_list[ctr%3].title;
-    title.style.width = title_list[ctr%3].width;
+    main_title.innerHTML = title_list[ctr%3].title;
+    main_title.style.width = title_list[ctr%3].width;
 
     // for changing image on rotating the shape
     setTimeout(() => {
@@ -41,4 +53,17 @@ btn.addEventListener('click', () => {
         image.classList.remove(image_class[(ctr-1)%3]);
         image.classList.add(image_class[ctr%3]);
     }, 1000);
+})
+
+trainerImage.addEventListener('click', () => {
+    trainerImage.classList.add('photo-rotate');
+    setTimeout(() => {
+        trainerCard.style.display = "none";
+        trainerDetailContainer.style.display = "flex";
+        setTimeout(() => {
+            profName.style.visibility = "visible";
+            profQualification.style.visibility = "visible";
+            profImage.style.visibility = "visible";
+        }, 1000)
+    }, 1500)
 })
